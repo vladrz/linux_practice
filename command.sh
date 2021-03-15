f20e479bc9b0dd0287684aced2b6acc5b79e3203
vlados <v.vlad.ryazanov@gmail.com>
2021-03-15 18:51:04 +0300

    go on

--- command.sh
+++ command.sh
@@ -1,1 +1,22 @@
-diff file1 file2
+diff file1 file2 // различие между файлами
+
+head -5 /etc/passwd
+tail +5 /etc/passwd
+
+
+sort // alphabet
+
+
+passwd // change pass
+chsh /bin/zsh
+
+
+Ctrl+A Перемещение курсора в начало строки
+Ctrl+E Перемещение курсора в конец строки
+
+
+/* input and output */
+command > file // overwriting file
+command >> file
+
+head /proc/cpuinfo | tr a-z A-Z // конвеер
